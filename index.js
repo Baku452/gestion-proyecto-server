598a1ef3dff46fb201dc394196f3c744bc0a2b02
Aldair <aiacos12@outlook.com>
2020-12-29 21:24:19 -0500

    ajustes deplyment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true}));
 
 //Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //IMportar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -27,6 +27,6 @@ app.use('/api/tareas', require('./routes/tareas'));
 //Definir la pagina principal
 
 //Arrancar la app
-app.listen(PORT, () => {
+app.listen(port, '0,0,0,0',() => {
     console.log(` El servidor esta funcionando en el puerto ${PORT}` );
 });
