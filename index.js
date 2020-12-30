2ed493ce6d9fda419a4aa9a48bd30348a745d93c
Aldair <aiacos12@outlook.com>
2020-12-29 22:23:00 -0500

    Mas cambios aaa

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const cors = require('cors');
 
 //Crear el servidor
 const app = express();
-// app.use(cors());
 
 //Conectar a la base de datos
 conectarDB();
 
 //Habilitar cors
+app.use(cors());
 
 // Habilitar express.json
 app.use(express.json({ extended: true}));
@@ -20,7 +20,7 @@ const port = process.env.PORT || 4000;
 //IMportar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 
