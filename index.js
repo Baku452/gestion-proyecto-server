10679ace110bae1048f97681bf5acd38f03f301b
Aldair <aiacos12@outlook.com>
2020-12-29 22:21:33 -0500

    CAMBIO PORT

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ conectarDB();
 app.use(express.json({ extended: true}));
 
 //Puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 //IMportar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
