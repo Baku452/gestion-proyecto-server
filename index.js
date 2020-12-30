e69d02e654c6886a99d920659dd4f4bb9852150d
Aldair <aiacos12@outlook.com>
2020-12-29 23:46:48 -0500

    port otra vez

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true}));
 
 //Puerto de la app
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
 
 //IMportar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -27,6 +27,5 @@ app.use('/api/tareas', require('./routes/tareas'));
 //Definir la pagina principal
 
 //Arrancar la app
-app.listen(port, '0.0.0.0',() => {
-    console.log(` El servidor esta funcionando en el puerto ${port}` );
-});
+const port = process.env.PORT || 4000
+app.listen(port, '0.0.0.0', () => console.log(`Server running on port: ${port}`))
